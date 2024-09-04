ff1d11372638332156c19861c6daaea3312b1b56
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-04 14:24:35 -0300

    criando pagina Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,72 @@
-export default function Login(){
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import Container from "../../components/Container";
+import Input from "../../components/Input";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um email válido")
+    .nonempty("O campo email é obrigatório"),
+  password: z.string().nonempty("O campo senha é obrigatório"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="Logo do site" className="w-full" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite o seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite a sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium">
+            Acessar
+          </button>
+        </form>
+      </div>
+    </Container>
+  );
 }
