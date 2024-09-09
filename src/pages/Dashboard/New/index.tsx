2313f262abb42132cc600bd9f42089cdc5142e13
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-09 11:34:13 -0300

    pagina novo carro

--- src/pages/Dashboard/New/index.tsx
+++ src/pages/Dashboard/New/index.tsx
@@ -1,7 +1,28 @@
-export default function New(){
-    return (
-        <div>
-            <h1>New</h1>
-        </div>
-    )
+import { FiUpload } from "react-icons/fi";
+import Container from "../../../components/Container";
+import DashboardHeader from "../../../components/PainelHeader";
+
+export default function New() {
+  return (
+    <Container>
+      <DashboardHeader />
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+        <button className="border-2 w-48 flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+          <div className="absolute cursor-pointer">
+            <FiUpload size={30} color="#000" />
+          </div>
+          <div className="cursor-pointer">
+            <input
+              type="file"
+              accept="image/*"
+              className="opacity-0 cursor-pointer"
+            />
+          </div>
+        </button>
+      </div>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2"></div>
+    </Container>
+  );
 }
