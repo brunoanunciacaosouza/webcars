3621e1b6e03b338e0757d50627df2685de8c69c8
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-10 18:04:33 -0300

    adicionando variaveis de ambiente

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQGT_BzNJL5vosR714AnpHmF4bWlVWRwI",
-  authDomain: "webcars-8be65.firebaseapp.com",
-  projectId: "webcars-8be65",
-  storageBucket: "webcars-8be65.appspot.com",
-  messagingSenderId: "225644604957",
-  appId: "1:225644604957:web:37557898c19236d77ce0f4",
-  measurementId: "G-40NNHGGCY6",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
