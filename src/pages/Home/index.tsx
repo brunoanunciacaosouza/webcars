3f03f2d6e6ca7664677d0c4ca57e6a673ead4785
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-09-10 14:17:01 -0300

    buscando carros

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,57 @@
+import { useEffect, useState } from "react";
 import Container from "../../components/Container";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { Link } from "react-router-dom";
+
+interface CarsProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export default function Home() {
+  const [cars, setCars] = useState<CarsProps[]>([]);
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        let listCars = [] as CarsProps[];
+
+        snapshot.forEach((doc) => {
+          listCars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            uid: doc.data().uid,
+            price: doc.data().price,
+            city: doc.data().city,
+            km: doc.data().km,
+            images: doc.data().images,
+          });
+        });
+
+        setCars(listCars);
+      });
+    }
+
+    loadCars();
+  }, []);
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -20,26 +71,34 @@ export default function Home() {
       </h1>
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg hover:scale-105 transition-all">
-          <img
-          className="w-full rounded-lg mb-2 max-h-72 p-1"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202408/20240801/mini-cooper-se-top-2p-eletrico-wmimagem12433565162.webp?s=fill&w=1920&h=1440&q=75"
-            alt="Carro"
-          />
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg hover:scale-105 transition-all">
+              <img
+                className="w-full rounded-lg mb-2 max-h-72 p-1"
+                src={car.images[0].url}
+                alt="Carro"
+              />
 
-          <p className="font-bold mt-1 mb-2 px-2">Mini-Cooper</p>
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">Ano 2018/2017 | 23.000 km</span>
-            <strong className="text-black font-medium text-xl">R$ 190.000</strong>
-          </div>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">
+                  Ano {car.year} | {car.km} kms
+                </span>
+                <strong className="text-black font-medium text-xl">
+                  R$ {car.price}
+                </strong>
+              </div>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
 
-          <div className="px-2 pb-2">
-            <span className="text-black">Campo Grande - MS</span>
-          </div>
-        </section>
+              <div className="px-2 pb-2">
+                <span className="text-black">{car.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
